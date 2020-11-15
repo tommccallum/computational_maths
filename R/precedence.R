12fb773a771e3543e6b77961c2803e00b9389322
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2020-11-15 22:55:55 +0000

    added precedence

---
+++ R/precedence.R
@@ -0,0 +1,16 @@
+# We get taught in schools BODMAS or PEDMAS
+# Brackets / Parentheses
+# Orders / Exponents
+# Division (left to right)
+# Multiplication
+# Addition (left to right)
+# Subtraction
+
+2+3*4 # 14
+(2+3)*4 # 20, if the addition was done before the multiplication
+2*2^3 # 16
+(2*2)^3 # 64, if the multiplication was done before the exponent
+
+# C precedence:
+# https://en.cppreference.com/w/c/language/operator_precedence
+# Brackets are the HIGHEST precedence.
