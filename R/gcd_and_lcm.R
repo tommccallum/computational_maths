17973466352ec91522625c10f8dba1fc8165acfc
Tom McCallum <40837110+tommccallum@users.noreply.github.com>
2020-11-17 09:20:13 +0000

    added gcd and lcm code

---
+++ R/gcd_and_lcm.R
@@ -0,0 +1,62 @@
+# Greatest common devisor
+gcd <- function(x, y) {
+    greatest_common_divisor <- NA
+    if(x > y) {
+        smaller_value = y
+    } else {
+        smaller_value = x
+    }
+    for(a_value in 1:smaller_value) {
+        # if x is divisible by i
+        # OR if y is divisible by i
+        if( (x %% a_value == 0) && (y %% a_value == 0)) {
+            greatest_common_divisor = a_value
+        }
+    }
+    return(greatest_common_divisor)
+}
+
+gcd(4,2)
+gcd(100,10)
+
+# Euclids version
+euclid_gcd <- function(x, y) {
+    while(y) {
+        temp = y
+        y = x %% y      # is x divisible by y, set y as the remainder
+        x = temp
+    }
+    return(x)
+}
+
+euclid_gcd(4,2)
+euclid_gcd(100,10)
+
+# Least common multiple
+lcm <- function(x, y) {
+    # choose the greater number
+    if(x > y) {
+        greater_value = x
+    } else {
+        greater_value = y
+    }   
+    least_common_multiple <- NA
+    while(TRUE) {
+        # is the larger value divisible by x and y
+        if((greater_value %% x == 0) && (greater_value %% y == 0)) {
+            least_common_multiple = greater_value
+            break
+        }
+        # if not increase
+        greater_value = greater_value + 1
+    }
+    return(least_common_multiple)
+}
+
+lcm(2,4)
+
+faster_lcm <- function(x,y) {
+    return ((x * y) / euclid_gcd(x,y))
+}
+
+faster_lcm(2,4)
